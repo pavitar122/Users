c3f8ecd688776a6df1b26e701e13677432dd3ec5
pavitar122 <pavitar3335@gmail.com>
2024-03-17 03:56:25 +0530

    static

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -124,14 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/0
 
-STATIC_ROOT = BASE_DIR / "static"
+
 STORAGES = {
-    # ...
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static')
