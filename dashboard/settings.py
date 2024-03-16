02ab79caff7fe110213904eb7974e8c9428fe724
pavitar122 <pavitar3335@gmail.com>
2024-03-17 04:09:16 +0530

    noise

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -135,6 +135,7 @@ STATICFILES_DIRS =[
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
